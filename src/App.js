62b707b415335d1981d8f28ba861d4bf3bdd3328
kcmodev <shchristensen3@gmail.com>
2021-04-26 17:18:53 -0500

    mobile formatting and profile information added

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
-// import logo from './logo.svg';
+import profile_picture from './profile_picture.jpg'
+import {intro_paragraph_text} from './text_variables'
 import './App.css';
-import background from './laptopBackgroundImage.jpg'
 
 function App() {
   return (
-    <div className="landingPage" style={{ backgroundImage: `url(${background})`}}>
-        {/*<div>*/}
-        {/*    <p>Hello World</p>*/}
-        {/*</div>*/}
+    <div className="landing_page">
+        <div className="profile_picture_container">
+            <img className="profile_picture" src={ profile_picture } alt="profile of the website author"/>
+        </div>
+        <div className="text_containers">
+            <div className="intro_container">
+                <p id="introduction">{intro_paragraph_text}</p>
+            </div>
 
-      {/*<header className="App-header">*/}
-        {/*  <img src={logo} className="App-logo" alt="logo" />*/}
-        {/*  <p>*/}
-        {/*    Edit <code>src/App.js</code> and save to reload.*/}
-        {/*  </p>*/}
-        {/*<a*/}
-        {/*  className="App-link"*/}
-        {/*  href="https://reactjs.org"*/}
-        {/*  target="_blank"*/}
-        {/*  rel="noopener noreferrer"*/}
-        {/*>*/}
-        {/*  Learn React*/}
-        {/*</a>*/}
-      {/*</header>*/}
+            <div className="skills_container">
+                <p id="skills">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+
+            <div className="social_container">
+                <p id="social">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+        </div>
     </div>
   );
 }
---
+++ src/text_variables.js
@@ -0,0 +1,4 @@
+export const intro_paragraph_text = "Hello! My name is Steve and I am a Full-Stack Developer. " +
+    "I graduated from Western Governors University with a degree in Computer Science. I am open to part-time, full-time," +
+    " or contract work. Please check below for links to a few of my projects, github repositories, and social media. Thank you for visiting" +
+    " and please feel free to contact me with any inquiries.";
