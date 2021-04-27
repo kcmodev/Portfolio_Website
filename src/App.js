24e21195e61a580ad199cc7207e0a138d0c6a7b7
kcmodev <shchristensen3@gmail.com>
2021-04-27 16:20:56 -0500

    added icons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import profile_picture from './profile_picture.jpg'
 import {intro_paragraph_text} from './text_variables'
+import python_icon from './python_logo.png'
+import javascript_icon from './javascript_logo.png'
+import java_icon from './java_logo.png'
+import react_icon from './react_logo.svg'
 import './App.css';
 
 function App() {
@@ -14,10 +18,19 @@ function App() {
             </div>
 
             <div className="skills_container">
-                <p id="skills">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <div className="python_container">
+                    <img className="python_icon" src={python_icon} alt='python programming language logo' />
+                    {/*<h1 className="caption">Python</h1>*/}
+                </div>
+                <div className="javascript_container">
+                    <img className="javascript_icon" src={javascript_icon} alt='javascript programming language logo'/>
+                </div>
+                <div className="java_container">
+                    <img className="java_icon" src={java_icon} alt='java programming language logo'/>
+                </div>
+                <div className="react_container">
+                    <img className="react_icon" src={react_icon} alt='react framework logo'/>
+                </div>
             </div>
 
             <div className="social_container">
--- src/text_variables.js
+++ src/text_variables.js
@@ -1,4 +1,6 @@
-export const intro_paragraph_text = "Hello! My name is Steve and I am a Full-Stack Developer. " +
-    "I graduated from Western Governors University with a degree in Computer Science. I am open to part-time, full-time," +
-    " or contract work. Please check below for links to a few of my projects, github repositories, and social media. Thank you for visiting" +
+export const intro_paragraph_text = "Hello! My name is Steve and I am a full stack developer. " +
+    "I graduated from Western Governors University with a bachelors degree in computer science. I am open to part-time, full-time," +
+    " or contract work. Please check below for links to a few of my personal projects and github repositories. Thank you for visiting" +
     " and please feel free to contact me with any inquiries.";
+
+export const skills_text = 'Below are two repositories I am currently working on. ';
