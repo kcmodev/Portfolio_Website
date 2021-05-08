fd8b8368d428add72b1d31fa31b872b8cd15d719
kcmodev <shchristensen3@gmail.com>
2021-05-08 15:19:36 -0500

    added python project styling

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,10 +1,10 @@
 import { Route, Switch } from 'react-router-dom';
 import {
-    java_projects_route,
-    password_generator_route,
-    python_projects_route,
-    react_projects_route,
-    about_me_route
+  java_projects_route,
+  password_generator_route,
+  python_projects_route,
+  react_projects_route,
+  about_me_route
 } from './route_names';
 import Password_Generator_Page from '../views/PasswordGeneratorPage';
 import Java_Projects_Page from '../views/JavaProjects';
@@ -13,33 +13,33 @@ import React_Projects_Page from '../views/ReactProjects';
 import AboutPage from '../views/AboutPage';
 
 const Routes = () => {
-    return (
-        <Route>
-            <Switch>
-                <Route
-                    exact
-                    path={password_generator_route}
-                    component={Password_Generator_Page}
-                />
-                <Route
-                    exact
-                    path={java_projects_route}
-                    component={Java_Projects_Page}
-                />
-                <Route
-                    exact
-                    path={python_projects_route}
-                    component={Python_Projects_Page}
-                />
-                <Route
-                    exact
-                    path={react_projects_route}
-                    component={React_Projects_Page}
-                />
-                <Route exact path={about_me_route} component={AboutPage} />
-            </Switch>
-        </Route>
-    );
+  return (
+    <Route>
+      <Switch>
+        <Route
+          exact
+          path={password_generator_route}
+          component={Password_Generator_Page}
+        />
+        <Route
+          exact
+          path={java_projects_route}
+          component={Java_Projects_Page}
+        />
+        <Route
+          exact
+          path={python_projects_route}
+          component={Python_Projects_Page}
+        />
+        <Route
+          exact
+          path={react_projects_route}
+          component={React_Projects_Page}
+        />
+        <Route exact path={about_me_route} component={AboutPage} />
+      </Switch>
+    </Route>
+  );
 };
 
 export default Routes;
--- src/views/LandingPage.js
+++ src/views/LandingPage.js
@@ -1,35 +1,47 @@
+import { useEffect } from 'react';
 import SkillDisplay from '../components/SkillDisplay';
 import ProfilePicture from '../components/ProfilePicture';
 import Navbar from '../components/Navbar';
 import Copyright from '../components/Copyright';
-// import GithubRepos from '../components/GithubRepos';
 
-export const intro_paragraph_text =
-    'Hello! My name is Steve and I am a Full Stack Software Developer. ' +
-    'I graduated from Western Governors University with a Bachelor of Science in Computer Science. ' +
-    'I am open to part-time, full-time, or contract work. ' +
-    'Please check below for links to a few of my personal projects and github repositories. Thank you for visiting ' +
-    'and please feel free to contact me with any inquiries.';
+const intro_paragraph_text =
+  'Hello! My name is Steve and I am a Full Stack Software Developer. ' +
+  'I graduated from Western Governors University with a Bachelor of Science in Computer Science. ' +
+  'I am open to part-time, full-time, or contract work. ' +
+  'Please check below for links to a few of my personal projects and github repositories. Thank you for visiting ' +
+  'and please feel free to contact me with any inquiries.';
+
+const ml_app_url = 'https://cvd-detection-wgu.herokuapp.com/';
 
 const LandingPage = () => {
-    return (
-        <>
-            <Navbar props={{ on_landing_page: true }} />
-            <ProfilePicture />
-            <div className="content_container">
-                <p id="section_text">{intro_paragraph_text}</p>
-            </div>
-            <SkillDisplay
-                props={{
-                    python: true,
-                    javascript: true,
-                    react: true
-                }}
-            />
-            {/*<GithubRepos />*/}
-            <Copyright />
-        </>
-    );
+  const request = new Request(ml_app_url, {
+    method: 'GET',
+    mode: 'no-cors',
+    cache: 'default'
+  });
+
+  // Send GET request to Heroku to wake app
+  useEffect(() => {
+    fetch(request).then(() => console.log('Request sent'));
+  });
+
+  return (
+    <>
+      <Navbar props={{ on_landing_page: true }} />
+      <ProfilePicture />
+      <div className="content_container">
+        <p id="section_text">{intro_paragraph_text}</p>
+      </div>
+      <SkillDisplay
+        props={{
+          python: true,
+          javascript: true,
+          react: true
+        }}
+      />
+      <Copyright />
+    </>
+  );
 };
 
 export default LandingPage;
