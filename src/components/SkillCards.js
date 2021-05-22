faeb50d3de729b7922604368391f08ef45b499cf
kcmodev <shchristensen3@gmail.com>
2021-05-22 13:00:15 -0500

    cleaned up skill cards

--- src/components/SkillCards.js
+++ src/components/SkillCards.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
