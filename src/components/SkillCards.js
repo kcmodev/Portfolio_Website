9e4f2c62ae8b0d8ec0c9e8fe021bd386571ae58d
kcmodev <shchristensen3@gmail.com>
2021-10-14 10:35:15 -0500

    fixed images, centered, and added links

--- src/components/SkillCards.js
+++ src/components/SkillCards.js
@@ -12,7 +12,7 @@ import { makeStyles } from '@material-ui/core/styles';
 // import python_logo from '../icons/python_logo.png';
 import password_generator_image from '../images/cryptography_image.jpeg'
 import api_project_image from '../images/api_project_image.jpeg'
-import { Container } from '@material-ui/core';
+import { Container, Typography } from '@material-ui/core';
 
 import {
   password_generator_route,
@@ -23,38 +23,39 @@ export const pass_gen_source_location =
   'https://github.com/kcmodev/Portfolio_Website/blob/main/src/components/PasswordGenerator.js';
 export const hvac_app_source_location =
   'https://github.com/kcmodev/pi_hvac_controller';
+
+const password_gen_desc_text = 'Password Generator'
+const password_gen_desc_text_2 = 'JavaScript, React, HTML, CSS';
 const password_generator_alt_text = 'Blue electronics cryptography image';
-const api_alt_text = 'Green electronics'
+const api_alt_text = 'Green electronics';
+const api_desc_text = 'Nest Thermostat API Integration';
+const api_desc_text_2 = 'Python, Flask, JavaScript, HTML, CSS'
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    // maxWidth: 375,
-    // backgroundColor: 'whitesmoke',
-    // background:
-    //   'linear-gradient( rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0.5) )',
-    // margin: theme.spacing(4),
-    // padding: theme.spacing(4)
+    color: 'whitesmoke',
+    justifySelf: 'center'
   },
-  // small: {
-  //   width: theme.spacing(3),
-  //   height: theme.spacing(3)
-  // },
-  // medium: {
-  //   width: theme.spacing(12),
-  //   height: theme.spacing(12)
-  // },
+  text: {
+    display: 'flex',
+    color: 'whitesmoke',
+    justifyContent: 'center'
+  },
   large: {
-    width: theme.spacing(50),
-    height: theme.spacing(50)
+    width: theme.spacing(60),
+    height: theme.spacing(45),
+    margin: theme.spacing(2),
+    transition: '0.5s',
+    '&:hover': {
+      opacity: 0.5
+    }
   },
   container: {
     display: 'flex',
     justifyContent: 'center',
-    flexWrap: 'wrap',
-    paddingTop: theme.spacing(4)
-  },
-  bottom: {
-    alignSelf: 'end'
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(4)
   }
 }));
 
@@ -63,8 +64,22 @@ export default function SkillCards() {
 
   return (
     <Container maxWidth={'md'} className={classes.container}>
-      <img className={classes.large} src={password_generator_image}  alt={password_generator_alt_text}/>
-      <img className={classes.large} src={api_project_image}  alt={api_alt_text}/>
+      <Container maxWidth={'sm'}>
+        <Link to={password_generator_route}>
+          <img className={classes.large} src={password_generator_image} alt={password_generator_alt_text}/>
+        </Link>
+        <Typography className={classes.text} variant={'h5'}>{password_gen_desc_text}</Typography>
+        <Typography className={classes.text} variant={'body1'}>{password_gen_desc_text_2}</Typography>
+      </Container>
+
+      <Container maxWidth={'sm'}>
+        <Link to={python_projects_route}>
+          <img className={classes.large} src={api_project_image} alt={api_alt_text} />
+        </Link>
+        <Typography className={classes.text} variant={'h5'}>{api_desc_text}</Typography>
+        <Typography className={classes.text} variant={'body1'}>{api_desc_text_2}</Typography>
+      </Container>
+
       {/*<Card className={classes.root}>*/}
       {/*  <CardMedia*/}
       {/*    className={classes.medium}*/}
