53a89eec2435e98d294dff44df31d8ff32709f78
kcmodev <shchristensen3@gmail.com>
2021-12-20 15:30:37 -0600

    added new images

---
+++ src/components/TextVariables.ts
@@ -0,0 +1,7 @@
+// Text Sizing
+export const title_variant_type = 'h6';
+export const subtitle_variant_type = 'body1';
+export const typography_center = 'center';
+
+// Text Colors
+export const text_color_white = 'whitesmoke';
